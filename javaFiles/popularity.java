82a608523c0515cc6e8c7d18c1bc0f4b0e08cb13
Apurva Pawar <theapurvapawar@gmail.com>
2013-02-01 19:32:04 +0530

    updated popularity class prototype

--- javaFiles/popularity.java
+++ javaFiles/popularity.java
@@ -2,15 +2,7 @@ import java.util.BitSet;
 
 public class popularity {
 	
-	BitSet zeroBitVector = new BitSet(8); 
-	BitSet oneBitVector = new BitSet(8);
-	
-	
-	popularity()
-	{
-	zeroBitVector.clear(0,7);
-	oneBitVector.set(0,7);
-	}
+	BitSet bitVector = new BitSet(8); 	
 	
 	BitSet updatePopularity(BitSet input,boolean updateValue)
 	{
@@ -26,5 +18,10 @@ public class popularity {
 		}
 		
 		return(input);
+	}
+	
+	BitSet setDefaultPopularity()
+	{
+		return(bitVector);
 	}
 }
